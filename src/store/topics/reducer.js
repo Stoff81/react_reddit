51729ef2b48ff4e06c3277c959927894e39f1ef7
Tom Stoffer <tom@touchsurgery.com>
2016-10-19 20:34:24 +0100

    Adding reducer, which turns the api response into the state we care about

--- src/store/topics/reducer.js
+++ src/store/topics/reducer.js
@@ -1,18 +1,17 @@
-// reducers hold the store's state (the initialState object defines it)
-// reducers also handle plain object actions and modify their state (immutably) accordingly
-// this is the only way to change the store's state
-// the other exports in this file are selectors, which is business logic that digests parts of the store's state
-// for easier consumption by views
-
+import * as types from './actionTypes';
 import Immutable from 'seamless-immutable';
 
 const initialState = Immutable({
-  topicsByUrl: {},
+  topicsByUrl: undefined,
   selectedTopicUrls: []
 });
 
 export default function reduce(state = initialState, action = {}) {
   switch (action.type) {
+    case types.TOPICS_FETCHED:
+      return state.merge({
+        topicsByUrl: action.topicsByUrl
+      });
     default:
       return state;
   }
